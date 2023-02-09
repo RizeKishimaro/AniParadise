6405478564c60bdec142df8882d9b4efa3528e39
RizeKishimaro <jokerlove261@gmail.com>
2023-02-09 20:17:57 +0630

    added web login with cute interface

---
+++ public/login/script.js
@@ -0,0 +1,45 @@
+const bsToastEl = document.querySelector("#liveToast")
+const userData = document.querySelector("#userData");
+const userBtn = document.querySelector("#userBtn");
+const userNameInput = document.querySelector("#userNameInput");
+const assistant = document.querySelector("#assistantImage");
+const toastEl = document.querySelector("#toastEl");
+const assistantName = document.querySelector("#assistantName");
+const data = new FormData(userData);
+const bsToast = new bootstrap.Toast(bsToastEl);
+let num = Math.round(Math.random()*1);
+userBtn.addEventListener("click",(e)=>{
+    if(userNameInput.value === ""){
+        bsToast.show();
+        e.preventDefault();
+    }else{
+        fetch("app.js",{
+            method: "POST",
+            body: data
+        }).then(response => response.json()).then(info => console.log(info)).catch(error => console.log(error));
+    }
+})
+const messages = ["Oh Please don't destroy the server <img src='/img/scared.gif' width='40px' alt='please?'>","please Noooooo..","Uhhhh Execuse Me?","このバカ！>_< <img src='/img/pout-girl.gif' width='40px' alt='i hate you'>"]
+let messageNum = Math.round(Math.random()*messages.length);
+const assistants = [
+    {
+        name: "Assistant Lum",
+        src: "/img/lum.jpg",
+        message: "Hey You Bad Guy!"
+    },
+    {
+        name: "Assistant nagatoro",
+        src: "/img/nagatoro.jpeg",
+        message: "Senpai won't like this."
+    }
+]
+assistantName.innerText = assistants[num].name
+assistant.setAttribute("src",assistants[num].src);
+toastEl.innerHTML = `<p>${messages[messageNum]}</p>`;
+bsToastEl.addEventListener("shown.bs.toast",()=>{
+    messageNum = Math.round(Math.random()*3);
+    num = Math.round(Math.random()*1);
+    assistant.setAttribute("src",assistants[num].src);
+    toastEl.innerHTML = `<p>${messages[messageNum]}</p>`;
+    assistantName.innerText = assistants[num].name;
+})
